7f840f9f05f01c75752cf37fb741b5b7182b6a8a
Colin Walters <walters@verbum.org>
2022-10-02 17:01:07 -0400

    Initial commit

---
+++ encap.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -euo pipefail
+
+ostreeref=$1
+shift
+image=$1
+shift
+
+runv () {
+    set -x
+    echo "$@"
+    $@
+}
+
+cd $(mktemp -d -p /var/tmp)
+runv skopeo inspect -n docker://${image} > inspect.json || true
+container_commit=
+if test -s inspect.json; then
+    container_commit=$(jq -r '.Labels["ostree.commit"]')
+fi
+
+mkdir repo
+ostree --repo=repo init --mode=bare-user
+cat /etc/ostree/remotes.d/fedora.conf >> repo/config
+runv ostree --repo=repo pull --commit-metadata-only fedora:$ostreeref
+current_commit=$(ostree --repo=repo rev-parse fedora:$ostreeref)
+
+if test "$current_commit" == "$container_commit"; then
+    echo "Generated container image is up to date at commit $current_commit"
+    exit 0
+fi
+
+runv ostree --repo=repo pull fedora:$ostreeref
+runv rpm-ostree compose container-encapsulate --format-version 1 \
+    --repo repo $current_commit oci:tmp
+# Retry since github actions networking flakes sometimes
+# TODO: add retries into skopeo, docker seems to do it
+for n in {0..2} max; do
+    if runv skopeo copy oci:tmp docker://$image; then
+        break
+    else
+        if test $n == max; then
+            exit 1
+        fi
+    fi
+done
